b616ac67070becc15fa2c7349c144f4c3675b167
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-01-19 22:18:41 +0000

    finished task 2

--- index.js
+++ index.js
@@ -64,9 +64,10 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(array){
+   return array.length === 31;
 }
+
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
