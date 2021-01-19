9aa366cbc4cabb0228a879a03bd472cebe2c49b4
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-01-19 22:23:12 +0000

    finished task 6

--- index.js
+++ index.js
@@ -137,8 +137,9 @@ Use the removeFlavorByName function below to do the following:
     HINT: You can use .splice() for this
 */
 
-function removeFlavorByName(/*your code here*/){
-    /*your code here*/
+function removeFlavorByName(flavors, flavor){
+    flavors.splice(flavors.indexOf(flavor), 1);
+    return flavors;
 }
 
 
